a959a821cff4d3b42dc197a4d6ff40cc548d1bdb
KW <136515956+practiceacck@users.noreply.github.com>
2025-08-16 01:00:26 -0700

    Add functions to get computer game choices and user game choices

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+
+// function that randomly returns "rock", "paper" or "scissors"
+const humanScore = 0;
+const computerScore = 0;
+
+function getComputerChoice(){
+    let random_num = Math.random();
+    console.log(random_num)
+    if (random_num <= 0.3) {
+        return "Rock";
+    } else if (random_num <= 0.6) {
+        return "Paper";
+    }else {
+        return "Scissors";
+    }
+}
+
+//function that allows user input to make their gameplay choice
+function getHumanChoice(){
+    let get_choice = prompt("Let's Play! Pick rock, paper or scissors and write your choice below.");
+    return get_choice;
+}
+
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
